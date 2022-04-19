be390799afb29bf58c792e6a35b3201d617ebe82
Jose Castro <jacastro023@gmail.com>
2022-04-19 17:40:02 -0500

    working exercise 1 and 2

--- exercises/exercise-1.py
+++ exercises/exercise-1.py
@@ -3,10 +3,15 @@
 # Write the code that:
 # 1. Prompts the user to enter a letter in the alphabet:
 #      Please enter a letter from the alphabet (a-z or A-Z): 
+letter = input('Please enter a letter from the aphabet (a-z or A-Z):').lower()
 # 2. Write the code that determines whether the letter entered is a vowel
+if letter in ('a', 'e', 'i', 'o', 'u'):
+
 # 3. Print one of following messages (substituting the letter for x):
 #      - The letter x is a vowel
 #      - The letter x is a consonant
-
+	print("The letter %s is a vowel." %letter)
+else:
+	print("The letter %s is a consonant." %letter) 
 # Hints:  Use the in operator to check if a character is in another string
 #         For example, if some_char in 'abc':
--- exercises/exercise-2.py
+++ exercises/exercise-2.py
@@ -3,7 +3,12 @@
 # Write the code that:
 # 1. Prompts the user to enter a phrase:
 #      Please enter a word or phrase: 
+phrase = input(' Please enter a word or phrase:').lower()
 # 2. Print the following message:
+while phrase != "quit":
+    if phrase != "quit":
+        print('What you entered is %s characters long' %len(phrase))
+        phrase = input(' Please enter a word or phrase:').lower()
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
 
