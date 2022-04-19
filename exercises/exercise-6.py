7906eb717d4b7bfa8630fdf050b3e6ff43050edb
Jose Castro <jacastro023@gmail.com>
2022-04-19 18:36:46 -0500

    working exercise 6

--- exercises/exercise-6.py
+++ exercises/exercise-6.py
@@ -12,7 +12,23 @@ day = input("Enter the day of the month: ").lower()
 #      Mar 20 - Jun 20: Spring
 #      Jun 21 - Sep 21: Summer
 #      Sep 22 - Dec 20: Fall
-
+# checking if its winter:
+if month in ('dec', 'jan', 'feb', 'mar'):
+    if month != 'dec' and month != 'mar' or month == 'dec' and int(day) >=21:
+        season = 'winter'
+    else:
+        season = 'fall'
+    if month == 'mar' and int(day) <= 19:
+            season = 'winter'
+    else:
+            season = 'spring'
+elif month in ('apr', 'may', 'jun'):
+    if month == 'jun' and int(day) <= 20:
+        season = 'spring'
+    else:
+        season = 'summer'
+else:
+    season = 'fall'
 # 4. Print the result as follows:
 #      <Mmm> <dd> is in <season> 
 print(f'{month} {day} is in {season}')
